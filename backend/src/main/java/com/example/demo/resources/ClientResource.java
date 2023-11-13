de0e2bbfa156d84d8f980462f36b2cc79d101d9e
Pedro Henrique <ph8584774@gmail.com>
2023-11-13 19:58:00 -0300

    Método de inserção e atualização finalizado

--- backend/src/main/java/com/example/demo/resources/ClientResource.java
+++ backend/src/main/java/com/example/demo/resources/ClientResource.java
@@ -34,13 +34,20 @@ public class ClientResource {
     @GetMapping(value = "/{id}")
     public ResponseEntity<ClientDTO> findById(@PathVariable Long id) {
         ClientDTO dto = service.findById(id);
-        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
-        return ResponseEntity.created(uri).body(dto);
+        return ResponseEntity.ok().body(dto);
     }
 
     @PostMapping
     public ResponseEntity<ClientDTO> insert(@RequestBody ClientDTO dto) {
         dto = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<ClientDTO> update(@PathVariable Long id, @RequestBody ClientDTO dto) {
+        dto = service.update(id, dto);
         return ResponseEntity.ok().body(dto);
     }
+    
 }
--- backend/src/main/java/com/example/demo/services/ClientService.java
+++ backend/src/main/java/com/example/demo/services/ClientService.java
@@ -4,6 +4,7 @@ import com.example.demo.dto.ClientDTO;
 import com.example.demo.entities.Client;
 import com.example.demo.repositories.ClientRepository;
 import com.example.demo.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.transaction.annotation.Transactional;
 import jdk.jfr.Category;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,7 +23,7 @@ public class ClientService {
     @Transactional(readOnly = true)
     public Page<ClientDTO> findAllPaged(PageRequest pageRequest) {
         Page<Client> list = repository.findAll(pageRequest);
-        return list.map(x -> new ClientDTO(x));
+        return list.map(ClientDTO::new);
     }
 
     @Transactional(readOnly = true)
@@ -36,9 +37,27 @@ public class ClientService {
     public ClientDTO insert(ClientDTO dto) {
         Client entity = new Client();
         entity.setName(dto.getName());
+        entity.setCpf(dto.getCpf());
+        entity.setIncome(dto.getIncome());
+        entity.setBirthDate(dto.getBirthDate());
+        entity.setChildren(dto.getChildren());
         entity = repository.save(entity);
         return new ClientDTO(entity);
     }
 
+    @Transactional
+    public ClientDTO update(Long id, ClientDTO dto) {
+        try {
+            Client entity = repository.getReferenceById(id);
+            entity.setName(dto.getName());
+            entity.setCpf(dto.getCpf());
+            entity.setIncome(dto.getIncome());
+            entity.setBirthDate(dto.getBirthDate());
+            entity.setChildren(dto.getChildren());
+            return new ClientDTO(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException("Id not found" + id);
+        }
+    }
 
 }
